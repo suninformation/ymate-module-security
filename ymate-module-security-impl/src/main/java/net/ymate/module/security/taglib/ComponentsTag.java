c17df87dbff2ef99052292f5ccb02fc0f24afd47
suninformation <suninformation@163.com>
2017-06-27 14:10:09 +0800

    完善组件和权限标签处理逻辑

--- ymate-module-security-impl/src/main/java/net/ymate/module/security/taglib/ComponentsTag.java
+++ ymate-module-security-impl/src/main/java/net/ymate/module/security/taglib/ComponentsTag.java
@@ -16,16 +16,36 @@
 package net.ymate.module.security.taglib;
 
 import net.ymate.framework.core.taglib.AbstractTagSupport;
+import net.ymate.module.security.ComponentsMeta;
+import org.apache.commons.lang.StringUtils;
 
 import javax.servlet.jsp.JspException;
 
 /**
+ * 加载权限菜单组件集合, 可以根据条件加载全部或指定唯一标识名称的菜单项
+ *
  * @author 刘镇 (suninformation@163.com) on 17/5/22 上午11:51
  * @version 1.0
  */
 public class ComponentsTag extends AbstractTagSupport {
 
+    /**
+     * 组件分组名称
+     */
+    private String name;
+
     protected Object doProcessTagData() throws JspException {
-        return null;
+        if (StringUtils.isNotBlank(name)) {
+            return ComponentsMeta.getComponents(name);
+        }
+        return ComponentsMeta.getComponents();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
--- ymate-module-security-impl/src/main/java/net/ymate/module/security/taglib/PermissionTag.java
+++ ymate-module-security-impl/src/main/java/net/ymate/module/security/taglib/PermissionTag.java
@@ -16,16 +16,115 @@
 package net.ymate.module.security.taglib;
 
 import net.ymate.framework.core.taglib.AbstractDataTagSupport;
+import net.ymate.module.security.IAuthenticatorFactory;
+import net.ymate.module.security.ISecurity;
+import net.ymate.module.security.IUserAuthenticator;
+import net.ymate.module.security.support.SecurityProxy;
+import org.apache.commons.lang.StringUtils;
 
 import javax.servlet.jsp.JspException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
+ * 判断当前用户是否拥有指定角色和权限
+ *
  * @author 刘镇 (suninformation@163.com) on 17/5/22 上午11:26
  * @version 1.0
  */
 public class PermissionTag extends AbstractDataTagSupport {
 
+    /**
+     * 是否拥有用户角色
+     */
+    private boolean userRole;
+
+    /**
+     * 是否拥有管理员角色
+     */
+    private boolean adminRole;
+
+    /**
+     * 是否拥有操作员角色
+     */
+    private boolean operatorRole;
+
+    /**
+     * 权限判断逻辑类型: AND或OR, 默认值: OR
+     */
+    private String logicType;
+
+    /**
+     * 需要验证的用户权限列表, 多个用"|"分隔
+     */
+    private String permissions;
+
     protected Object doProcessTagData() throws JspException {
+        IAuthenticatorFactory _factory = net.ymate.module.security.Security.get().getModuleCfg().getAuthenticatorFactory();
+        if (_factory != null) {
+            IUserAuthenticator _authenticator = _factory.createUserAuthenticatorIfNeed();
+            if (_authenticator != null) {
+                // 进行用户角色判断
+                List<ISecurity.Role> _roles = new ArrayList<ISecurity.Role>();
+                if (userRole) {
+                    _roles.add(ISecurity.Role.USER);
+                }
+                if (adminRole) {
+                    _roles.add(ISecurity.Role.ADMIN);
+                }
+                if (operatorRole) {
+                    _roles.add(ISecurity.Role.OPERATOR);
+                }
+                //
+                String[] _permissions = StringUtils.split(StringUtils.trimToEmpty(permissions), "|");
+                //
+                boolean _returnFlag = SecurityProxy.containsUserRole(_roles.toArray(new ISecurity.Role[0]), _authenticator);
+                // 进行用户权限判断
+                if (_returnFlag && SecurityProxy.containsUserPermissions(ISecurity.LogicType.valueOf(StringUtils.defaultIfBlank(logicType, "OR").toUpperCase()), _permissions, _authenticator)) {
+                    return true;
+                }
+            }
+        }
         return null;
+    }
+
+    public boolean isUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(boolean userRole) {
+        this.userRole = userRole;
+    }
+
+    public boolean isAdminRole() {
+        return adminRole;
+    }
+
+    public void setAdminRole(boolean adminRole) {
+        this.adminRole = adminRole;
+    }
+
+    public boolean isOperatorRole() {
+        return operatorRole;
+    }
+
+    public void setOperatorRole(boolean operatorRole) {
+        this.operatorRole = operatorRole;
+    }
+
+    public String getLogicType() {
+        return logicType;
+    }
+
+    public void setLogicType(String logicType) {
+        this.logicType = logicType;
+    }
+
+    public String getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(String permissions) {
+        this.permissions = permissions;
     }
 }
